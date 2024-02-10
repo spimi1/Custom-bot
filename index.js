8b6751b4f052808cf03fc38afbc728037ff219c6
spimi1 <158826509+spimi1@users.noreply.github.com>
2024-02-10 10:00:32 +0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["W grze **ASTROSMP.PL**"];
 
 
 let currentIndex = 0;
